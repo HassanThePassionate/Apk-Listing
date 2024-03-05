d5e2b3665330626e4769add1fb24c15c6cc741e9
alihassan8989 <maqsoodia777@gmail.com>
2024-03-05 15:35:48 +0500

    first commit

--- src/app/Components/MiniCardbtn.js
+++ src/app/Components/MiniCardbtn.js
@@ -22,13 +22,14 @@ const MiniCardbtn = ({ Instance }) => {
             id="IconChangeColor"
             height="12"
             width="12"
+            className="slider-svg"
           >
             <path
               d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
               id="mainIconPathAttribute"
               strokeWidth="0.5"
               stroke="#ff0000"
-              fill="#000000"
+              className="slider-btn"
             ></path>
           </svg>
         </button>
@@ -39,13 +40,13 @@ const MiniCardbtn = ({ Instance }) => {
             id="IconChangeColor"
             height="12"
             width="12"
+            className="slider-svg"
           >
             <path
               d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
               id="mainIconPathAttribute"
               strokeWidth="0.5"
               stroke="#ff0000"
-              fill="#000000"
             ></path>
           </svg>
         </button>
