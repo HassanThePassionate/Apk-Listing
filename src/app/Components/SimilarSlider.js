7ff31f31d88b9c514fda40295563f68d4fb92ea1
alihassan8989 <maqsoodia777@gmail.com>
2024-03-06 17:47:57 +0500

    first commit

---
+++ src/app/Components/SimilarSlider.js
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import style from "../style/Postdeatail.module.css";
+import Image from "next/image";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+const SimilarSlider = () => {
+  const [swiperInstance, setSwiperInstance] = useState(null);
+  const goNexts = () => {
+    if (swiperInstance) {
+      swiperInstance.slideNext();
+    }
+  };
+
+  const goPrevs = () => {
+    if (swiperInstance) {
+      swiperInstance.slidePrev();
+    }
+  };
+
+  return (
+    <>
+      <Swiper
+        modules={[Navigation]}
+        onSwiper={(swiper) => setSwiperInstance(swiper)}
+        navigation={{
+          nextEl: "#nexts",
+          prevEl: "#prevs",
+        }}
+        className="mySwiper"
+      >
+        <div className={style.sliderControls}>
+          <button onClick={goPrevs}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              id="IconChangeColor"
+              height="12"
+              width="12"
+              className="slider-svg"
+            >
+              <path
+                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+                id="mainIconPathAttribute"
+                strokeWidth="0.5"
+                stroke="#ff0000"
+              ></path>
+            </svg>
+          </button>
+          <button onClick={goNexts}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              id="IconChangeColor"
+              height="12"
+              width="12"
+            >
+              <path
+                d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+                id="mainIconPathAttribute"
+                strokeWidth="0.5"
+                stroke="#ff0000"
+                className="slider-svg"
+              ></path>
+            </svg>
+          </button>
+        </div>
+        <div className={style.imageWrapper}>
+          <SwiperSlide
+            style={{
+              marginRight: "10px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/wq8j0eajviPxSZHa51zj2VtkDhT2Tovdm6ByVQY99offUKmhJGVmbX-uSzlhIt2Ffg=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              marginRight: "10px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/LnoB2O-eMnpDUdFQ0fTqaKDnWUSPMYWtr7vV0Q_PUX6p8vOWfp-j9xmdxtO-yTjG2QI=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              marginRight: "1px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/hdDZNpPhwN13dQnV8bIjL7jokrJVB2xomGc18oHtomavesMzDQrMhocjsImPRaFpRw_Z=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              marginRight: "1px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/tkDyfbAGHnrHb9j4-Sbk1RyK_GtkHmmB-9VfHCVGg6p2hGJDmCZTIMEUDWXTA3M1S9c=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              marginRight: "1px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/5Ibaqy_24ticqqLkAMQcRrvW--4K4uhDLqLoVQsYd7cGNcVLIJAAYCTBdArqM0gPTQ=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide
+            style={{
+              marginRight: "1px",
+            }}
+          >
+            <div className={style.sliderimage}>
+              <Image
+                src="https://play-lh.googleusercontent.com/tMX61_jmONRF2H7XLdoRjTEGEzAaCA78M5m81w2m_XC8OmsHUKFEdYJ9ZQl_MPHmRWU=w2560-h1440-rw"
+                alt="img"
+                width={736}
+                height={414}
+              />
+            </div>
+          </SwiperSlide>
+        </div>
+      </Swiper>
+    </>
+  );
+};
+
+export default SimilarSlider;
