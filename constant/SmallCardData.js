6b64847ef6f172f313d4ac99b4cb9f6886feb533
alihassan8989 <maqsoodia777@gmail.com>
2024-03-07 17:30:01 +0500

    first commit

--- constant/SmallCardData.js
+++ constant/SmallCardData.js
@@ -2,7 +2,7 @@
 export const data = [
   {
     // Image URL of the app
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     // Title of the app
     title: "Temu: Shop Like a Billionaire",
     // Category of the app
@@ -11,67 +11,67 @@ export const data = [
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s64-rw",
+    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s512-rw",
     title: "Tiktok",
     catagory: "Social",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s256-rw",
+    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s512-rw",
     title: "WhatsApp Messenger",
     catagory: "Communication",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     title: "Temu: Shop Like a Billionaire",
     catagory: "Shopping",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     title: "Temu: Shop Like a Billionaire",
     catagory: "Shopping",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s64-rw",
+    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s512-rw",
     title: "Tiktok",
     catagory: "Social",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s256-rw",
+    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s512-rw",
     title: "WhatsApp Messenger",
     catagory: "Communication",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     title: "Temu: Shop Like a Billionaire",
     catagory: "Shopping",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     title: "Temu: Shop Like a Billionaire",
     catagory: "Shopping",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s64-rw",
+    img: "https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=s512-rw",
     title: "Tiktok",
     catagory: "Social",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s256-rw",
+    img: "https://play-lh.googleusercontent.com/bYtqbOcTYOlgc6gqZ2rwb8lptHuwlNE75zYJu6Bn076-hTmvd96HH-6v7S0YUAAJXoJN=s512-rw",
     title: "WhatsApp Messenger",
     catagory: "Communication",
     rating: 4.7,
   },
   {
-    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s64-rw",
+    img: "https://play-lh.googleusercontent.com/9v08ai07I8aoFLj5M-90nzWPpvyNzOVgA2ZWF9avdW7oS8L9YqF9trVI44SUn2qGTA0=s512-rw",
     title: "Temu: Shop Like a Billionaire",
     catagory: "Shopping",
     rating: 4.7,
