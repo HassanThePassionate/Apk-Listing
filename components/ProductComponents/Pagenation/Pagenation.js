18a056dc46ee5ec1e681ddfd23807f2341024524
alihassan8989 <maqsoodia777@gmail.com>
2024-03-13 00:26:36 +0500

    Edit

--- components/ProductComponents/Pagenation/Pagenation.js
+++ components/ProductComponents/Pagenation/Pagenation.js
@@ -1,25 +1,9 @@
-"use client";
-import React, { useState } from "react";
-import ReactPaginate from "react-paginate";
+import Buttons from "./Buttons/Buttons";
 import style from "./Pagenation.module.css";
 const Pagenation = () => {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const handlePageClick = (selectedPage) => {
-    setCurrentPage(selectedPage.selected);
-  };
   return (
     <div className={style.pagination}>
-      <ReactPaginate
-        pageCount={10} // Set the total number of pages
-        pageRangeDisplayed={3} // Set the number of page links to display
-        marginPagesDisplayed={1} // Set the number of pages to display on the edges
-        onPageChange={handlePageClick} // Handle page change event
-        containerClassName={"pagination"}
-        activeClassName={"active"}
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-      />
+      <Buttons />
     </div>
   );
 };
