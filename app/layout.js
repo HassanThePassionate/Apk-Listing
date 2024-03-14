3abf1938033fd5fd16a94ea7c05ce308f8239432
alihassan8989 <maqsoodia777@gmail.com>
2024-03-14 20:27:44 +0500

    Edit

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
-  display: "swap",
 });
 
 export const metadata = {
