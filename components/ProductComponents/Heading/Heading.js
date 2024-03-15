20a56749b1e7f872a2b7cf79dfa947d06cd29813
alihassan8989 <maqsoodia777@gmail.com>
2024-03-16 01:43:46 +0500

    Edit

--- components/ProductComponents/Heading/Heading.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import style from "./Heading.module.css";
-const Heading = () => {
-  return <h2 className={style.heading}>Product Found (63)</h2>;
-};
-
-export default Heading;
