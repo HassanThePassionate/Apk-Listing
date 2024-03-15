1e635dfe47f166fd7e7c5e8bb812faef653df67b
alihassan8989 <maqsoodia777@gmail.com>
2024-03-16 02:13:02 +0500

    edit

--- app/download/page.js
+++ app/download/page.js
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer/Footer";
 import style from "./page.module.css";
 
 import Apk from "@/components/PostComponents/Apk/Apk";
-import Download from "@/components/DownloadComponents/Download";
+import Download from "@/components/Downloadcomponents/Download";
 import Similar from "@/components/PostComponents/Similar/Similar";
 
 const page = () => {
