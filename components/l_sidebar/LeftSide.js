7322f7aba84beeffabdde271f05a3bbd8dc2c798
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-24 17:18:52 +0500

    Update

--- components/l_sidebar/LeftSide.js
+++ components/l_sidebar/LeftSide.js
@@ -1,8 +1,13 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import style from "./l_sidebar.module.css";
 const LeftSide = () => {
+  const [opens, setOpens] = useState(true);
+  const openhandle = () => {
+    setOpens(!opens);
+  };
   return (
-    <div>
+    <div className={style.wrap}>
       <ul
         className='flex flex-col  space-y-3  font-bold w-full'
         id={style.list}
@@ -125,7 +130,10 @@ const LeftSide = () => {
           </span>
           <span className='hidden md:block'>Profile</span>
         </li>
-        <li className='flex md:justify-start items-center gap-3 justify-center mr-4 md:w-fit   hover:cursor-pointer px-5 py-3 hover:rounded-full'>
+        <li
+          className='flex md:justify-start items-center gap-3 justify-center mr-4 md:w-fit   hover:cursor-pointer px-5 py-3 hover:rounded-full'
+          onClick={openhandle}
+        >
           <span>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -136,9 +144,49 @@ const LeftSide = () => {
               <path d='M280-420q25 0 42.5-17.5T340-480q0-25-17.5-42.5T280-540q-25 0-42.5 17.5T220-480q0 25 17.5 42.5T280-420Zm200 0q25 0 42.5-17.5T540-480q0-25-17.5-42.5T480-540q-25 0-42.5 17.5T420-480q0 25 17.5 42.5T480-420Zm200 0q25 0 42.5-17.5T740-480q0-25-17.5-42.5T680-540q-25 0-42.5 17.5T620-480q0 25 17.5 42.5T680-420ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z' />
             </svg>
           </span>
-          <span className='hidden md:block'>More</span>
+          <span className='hidden md:block  '>More</span>
         </li>
-        <li></li>
+        <div className={`${opens ? "opacity-0" : "opacity-1"} transition-all`}>
+          <li className='flex md:justify-start items-center gap-3 justify-center mr-4 md:w-fit  hover:cursor-pointer px-5 py-3 hover:rounded-full'>
+            <span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                height='36'
+                viewBox='0 -960 960 960'
+                width='30'
+              >
+                <path d='M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z' />
+              </svg>
+            </span>
+            <span className='hidden md:block'>Communities</span>
+          </li>
+          <li className='flex md:justify-start items-center gap-3 justify-center mr-4 md:w-fit  hover:cursor-pointer px-5 py-3 hover:rounded-full'>
+            <span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                height='36'
+                viewBox='0 -960 960 960'
+                width='30'
+              >
+                <path d='m400-570 80-40 80 40v-190H400v190ZM280-280v-80h200v80H280Zm-80 160q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-640v560-560Zm0 560h560v-560H640v320l-160-80-160 80v-320H200v560Z' />
+              </svg>
+            </span>
+            <span className='hidden md:block'>Premium</span>
+          </li>
+          <li className='flex md:justify-start items-center gap-3 justify-center mr-4 md:w-fit  hover:cursor-pointer px-5 py-3 hover:rounded-full'>
+            <span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                height='36'
+                viewBox='0 -960 960 960'
+                width='30'
+              >
+                <path d='M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z' />
+              </svg>
+            </span>
+            <span className='hidden md:block'>Profile</span>
+          </li>
+        </div>
       </ul>
     </div>
   );
