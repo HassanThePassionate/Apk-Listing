a7263ff498034426b1c09d880d864e5b6d454494
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-24 18:14:34 +0500

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "clipground.com",
       },
+      {
+        protocol: "https",
+        hostname: "pic.clubic.com",
+      },
     ],
   },
 };
