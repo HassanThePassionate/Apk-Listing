0c3d4b25addbc6d7adb6d05b32a022418f3ff91f
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-25 10:54:55 +0500

    Update

--- constant/data.js
+++ constant/data.js
@@ -1,36 +1,36 @@
 export const data = [
   {
-    img: "https://pic.clubic.com/v1/images/2070885/raw?fit=smartCrop&width=120&height=120&hash=9cc48132e58409f09bdccd13b275cf59492f9d1f",
+    img: "/1.webp",
     des: "Adobe launches its new free PDF reader for everyone",
     title: "Adobe Acrobat Reader DC",
   },
   {
-    img: "https://pic.clubic.com/v1/images/2202561/raw?fit=smartCrop&width=120&height=120&hash=0add87fc6377de12d2f3cbd68fceffc0e23a49c3",
+    img: "/2.webp",
     des: "A modified and more complete WhatsApp",
     title: "GBWhatsApp",
   },
   {
-    img: "https://pic.clubic.com/v1/images/2059296/raw?fit=smartCrop&width=120&height=120&hash=25fd070d0e446514dd645f37a496d2ac0af250bc",
+    img: "/3.webp",
     des: "WinRAR is an essential archiving and compression tool for Windows,",
     title: "WinRAR",
   },
   {
-    img: "https://pic.clubic.com/v1/images/2202915/raw?fit=smartCrop&width=120&height=120&hash=1eebcb7cbbfaaa18c3180163a6ae61740c250a60",
+    img: "/4.webp",
     des: "The official Google Play store provides access to numerous content: games, utilities",
     title: "Google Play Store",
   },
   {
-    img: "https://pic.clubic.com/v1/images/2059311/raw?fit=smartCrop&width=120&height=120&hash=fe66f36fe3cdb95653764227136830dec71f6fee",
+    img: "/5.webp",
     des: "CCleaner is a PC cleaning tool that removes unnecessary files, frees up disk space",
     title: "CCleaner",
   },
   {
-    img: "https://pic.clubic.com/v1/images/1502578/raw?fit=smartCrop&width=120&height=120&hash=0634168b40b5e0b93533f6c0d109b22cb5eaf44f",
+    img: "/6.webp",
     des: "Simple and free conversion of audio and video files",
     title: "Factory format",
   },
   {
-    img: "https://pic.clubic.com/v1/images/2068863/raw?fit=smartCrop&width=120&height=120&hash=033ae9f0ebd8f0f47f616cd3ad4d5cf00bcf7a87",
+    img: "/7.webp",
     des: "Videoconferencing for 100 users, in HD and free.",
     title: "Zoom",
   },
